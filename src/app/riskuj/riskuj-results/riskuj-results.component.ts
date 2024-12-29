af0738808784c88f9caf3d1f602b75ada18f867c
Martin Kostohryz <martin.kostohryz@protonmail.com>
2024-12-29 09:37:24 +0100

    update results component

--- src/app/riskuj/riskuj-results/riskuj-results.component.ts
+++ src/app/riskuj/riskuj-results/riskuj-results.component.ts
@@ -2,11 +2,35 @@ import { Component } from '@angular/core';
 import { RiskujService, Team } from "../riskuj.service";
 import { CommonModule } from "@angular/common";
 
+interface PlaceColor {
+    index: number;
+    color: string;
+}
+
+const PLACE_COLORS: PlaceColor[] = [
+    {
+        index: 0,
+        color: "#ffae00"
+    }, {
+        index: 1,
+        color: "#cecece"
+    }, {
+        index: 2,
+        color: "#b48429"
+    },
+]
+
+const DEFAULT_PLACE_COLOR: PlaceColor = {
+    index: -1,
+    color: "#807659"
+}
+
+
 @Component({
-  selector: 'app-riskuj-results',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './riskuj-results.component.html',
+    selector: 'app-riskuj-results',
+    standalone: true,
+    imports: [ CommonModule ],
+    templateUrl: './riskuj-results.component.html',
 })
 export class RiskujResultsComponent {
     teams: Team[] = [];
@@ -16,6 +40,13 @@ export class RiskujResultsComponent {
     }
 
     getTeamsOrder(): Team[] {
-        return this.teams.sort((t1,t2) => t1.points-t2.points);
+        return this.teams.sort((t1, t2) => t1.points - t2.points);
+    }
+
+    getPlaceColor(index: number): string {
+        if (index < PLACE_COLORS.length) {
+            return PLACE_COLORS[index].color;
+        }
+        return DEFAULT_PLACE_COLOR.color;
     }
 }
